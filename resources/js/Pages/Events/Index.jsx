ccd60c163ca8e02a6f9920ec8edfd26a5e2669db
VictorG0234 <gargonvh@gmail.com>
2025-12-06 21:14:13 -0600

    Cambios de colores

--- resources/js/Pages/Events/Index.jsx
+++ resources/js/Pages/Events/Index.jsx
@@ -41,7 +41,8 @@ export default function Index({ auth, events }) {
                     </h2>
                     <Link
                         href={route('events.create')}
-                        className="inline-flex items-center px-4 py-2 bg-blue-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-blue-700 focus:bg-blue-700 active:bg-blue-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+                        className="inline-flex items-center px-4 py-2 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest focus:outline-none focus:ring-2 focus:ring-offset-2 transition ease-in-out duration-150"
+                        style={{ backgroundColor: '#D22730' }}
                     >
                         <PlusIcon className="w-4 h-4 mr-2" />
                         Nuevo Evento
